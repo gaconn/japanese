ba7d3824909d9393ba85eab7b1c408e15c49af2a
quan12xz <quan12xz@gmail.com>
2023-06-27 16:29:20 +0700

    update practice

---
+++ data/images/test.php
@@ -0,0 +1,16 @@
+<?php
+
+$list_file = scandir("./214");
+
+
+// foreach ($list_file as $key => $value) {
+//     $arrName = explode(".", $value);
+//     $newName = hex2bin($arrName[0]) . ".gif";
+//     rename("./214/$value", "./214/$newName");
+// }
+
+// echo "done";
+
+echo "<script type=\"script/javascript\">
+'二'.charCodeAt(i).toString(16);
+</script>";
